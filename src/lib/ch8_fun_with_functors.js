d540085aaca0c9cd566c7baef2b8998b3faeb890
voidSatisfaction <lourie@naver.com>
2017-08-15 02:42:31 +0900

    feat: add content on functors

--- src/lib/ch8_fun_with_functors.js
+++ src/lib/ch8_fun_with_functors.js
@@ -11,11 +11,11 @@ Container.prototype.map = function(fn) {
   return Container.of(fn(this.value));
 };
 
+// MayBe
 export const MayBe = function(val) {
   this.value = val;
 };
 
-// MayBe
 MayBe.of = function(val) {
   return new MayBe(val);
 };
@@ -26,4 +26,34 @@ MayBe.prototype.isNothing = function() {
 
 MayBe.prototype.map = function(fn) {
   return this.isNothing() ? MayBe.of(null) : MayBe.of(fn(this.value));
+};
+
+// Either
+const Nothing = function(val) {
+  this.value = val;
+};
+
+Nothing.of = function(val) {
+  return new Nothing(val);
+};
+
+Nothing.prototype.map = function() {
+  return this;
+};
+
+export const Some = function(val) {
+  this.value = val;
+};
+
+Some.of = function(val) {
+  return new Some(val);
+};
+
+Some.prototype.map = function(fn) {
+  return Some.of(fn(this.value));
+};
+
+export const Either = {
+  Some,
+  Nothing
 };
